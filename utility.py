bd6166a9194d4897c1a74479cc5006d8849b5efb
aa8796-tech <aa8796@fayoum.edu.eg>
2026-05-29 16:57:02 +0300

    Add files via upload

--- utility.py
+++ utility.py
@@ -1,8 +1,14 @@
+import os
+import shutil
+
 # =========================
 # Terminal Utilities
 # =========================
 def clear_screen() -> None:
     os.system("cls" if os.name == "nt" else "clear")
 
-def pause() -> Nonw:
+def pause() -> None:
     input("Press Enter to continue...")
+
+def get_terminal_width() -> int:
+    return shutil.get_terminal_size((80, 20)).columns
